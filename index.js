a442e16be8bdcb8796373844f77ee3cdcf0b3281
Aly Merchant <aly.merchant@ec.gc.ca>
2015-08-18 09:55:42 -0400

    refactor(global): fix style issues

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 'use strict';
 var through = require('through2');
-var fs = require('fs');
-var csv = require('csv');
 var csvParse = require('csv-parse');
 var gutil = require('gulp-util');
-var PluginError = gutil.PluginError;
 var File = require('vinyl');
 
-module.exports = function (options) {
+module.exports = function(options) {
     options = options || {};
 
     // stringifies JSON and makes it pretty if asked
@@ -21,18 +18,12 @@ module.exports = function (options) {
 
     // parse array to JSON object and write to JSON file
     function parseArray(csvArray, task) {
-        var jsonObj;
-        var lang;
-        var jsfile;
 
         for (var i = 2; i < csvArray[0].length; i++) {
-            // use the same name for the translation files
-            var file = 'translation.json';
-
-            // initiate JSON file for output[0][i] language here
-            lang = csvArray[0][i];
-            // initiate JSON string to write here
-            jsonObj = {};
+            var file = 'translation.json'; // use the same name for the translation files
+            var lang = csvArray[0][i]; // get language from the CSV header row
+            var jsonObj = {}; // JSON object to be created
+            var jsfile;
 
             for (var j = 0; j < csvArray.length; j++) {
                 // append to JSON string
@@ -42,15 +33,20 @@ module.exports = function (options) {
 
                 if (subkeyArray) {
                     var subkey = subkeyArray[1];
-                    var superkey = subkeyArray[0]
+                    var superkey = subkeyArray[0];
 
+                    // temp fix for !== object, should get rewritten for #6
+                    // "deep key nesting"
                     value = csvArray[j][i];
-                    if (!jsonObj[superkey]) {
+                    if (typeof jsonObj[superkey] !== 'object') {
                         jsonObj[superkey] = {};
                     }
+
+                    //console.log(superkey+', '+subkey+', '+stringify(jsonObj[superkey]));
                     jsonObj[superkey][subkey] = value;
                 } else {
                     value = csvArray[j][i];
+
                     //console.log(key, value);
                     jsonObj[key] = value;
                 }
@@ -58,14 +54,14 @@ module.exports = function (options) {
                 jsfile = new File({
                     cwd: '/',
                     path: '/' + lang + '/' + file, // put each translation file in a folder
-                    contents: new Buffer(stringify(jsonObj))
+                    contents: new Buffer(stringify(jsonObj)),
                 });
             }
 
             // do not write files from the gulp plugin itself
             // create a file object and push it back to through stream
             // so main gulpfile
-            task.push(jsfile)
+            task.push(jsfile);
         }
     }
 
@@ -79,8 +75,8 @@ module.exports = function (options) {
         }
     }
 
-    return through.obj(function (file, enc, cb) {
-        var task = this; // task is a reference to the through stream
+    return through.obj(function(file, enc, cb) {
+        var _this = this; // task is a reference to the through stream
 
         if (file.isNull()) {
             cb(null, file);
@@ -92,33 +88,33 @@ module.exports = function (options) {
             var csvArray = [];
             var parser = csvParse();
 
-            parser.on('readable', function () {
-                var record;
-                while (record = parser.read()) {
+            parser.on('readable', function() {
+                var record = parser.read();
+                while (record) {
                     csvArray.push(record);
+                    record = parser.read();
                 }
             });
 
-            parser.on('error', function (err) {
+            parser.on('error', function(err) {
                 //console.log('on error', err.message);
                 this.emit('error', new gutil.PluginError('gulp-i18n-csv', err));
             });
 
-            parser.on('finish', function () {
+            parser.on('finish', function() {
                 //console.log('on finish');
-                parseArray(csvArray, task);
+                parseArray(csvArray, _this);
                 parser.end();
                 cb();
             });
 
             file.contents.pipe(parser);
-        }
-        else {
+        } else {
             // BUFFER BLOCK
             try {
                 csvParse(file.contents.toString('utf-8'),
-                    function (err, output) {
-                        parseArray(output, task);
+                    function(err, output) {
+                        parseArray(output, _this);
                         cb();
                     });
             } catch (err) {

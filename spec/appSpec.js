477328f1a4dda23ac08f8faa236d8f8de38226b6
Aly Merchant <aly.merchant@ec.gc.ca>
2015-09-22 15:52:40 -0400

    test(core): add basic two language test
    Relates #2

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,23 +1,32 @@
 /* jshint jasmine: true */
 'use strict';
-var assert = require('assert');
-var gutil = require('gulp-util');
-var i18nCsv = require('./');
+var File = require('vinyl');
+var i18nCsv = require('../');
 
-it('should ', function (cb) {
-    var stream = i18nCsv();
+describe("Buffer mode",function () {
 
-    stream.on('data', function (file) {
-        assert.strictEqual(file.contents.toString(), 'unicorns');
+    beforeEach(function() {
+        this.plugin = i18nCsv();
     });
 
-    stream.on('end', cb);
+    it('should work for a basic En/Fr case', function (cb) {
+        var data = new File({contents: new Buffer('headers,lang,en,fr\nuseless notes,key,enval,frval\nuseless notes,key2,enval,frval')});
+        var files = [];
+        this.plugin.write(data);
+        this.plugin.end();
+        this.plugin.on('data', function (file) {
+            files.push(file);
+            try {
+                JSON.parse(file.contents);
+            } catch (e) {
+                fail('Could not parse the generated file');
+            }
+        });
+        this.plugin.on('end', function () {
+            expect(files.length).toBe(2);
+            cb();
+        });
 
-    stream.write(new gutil.File({
-        base: __dirname,
-        path: __dirname + '/file.ext',
-        contents: new Buffer('unicorns'),
-    }));
+    });
 
-    stream.end();
 });
